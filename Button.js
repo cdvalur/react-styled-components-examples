3e39ba9276235940648dd0f7a7331301d1794a06
cdvalur <dchandrasekhar426@gmail.com>
2019-05-09 12:32:43 +0530

    1 file modified, 1 added

---
+++ Button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+width: 200px;
+height: 35px;
+background-color: pink;
+color:black;
+border: 1 px solid green;
+font-weight: bold;
+`
